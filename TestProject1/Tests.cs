c8432a8a1b840d45e32942fa881b98137034cf32
runlei <rlwang@thoughtworks.com>
2019-05-08 18:51:05 +0800

    [Commit 06][Test Pass]: When numbert is times of 3 and 5 should return FizzBuzz function Complete and Test pass

--- TestProject1/Tests.cs
+++ TestProject1/Tests.cs
@@ -8,11 +8,14 @@ namespace TestProject1
     {
         public string GetValue(int num)
         {
+            if (num % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
             if (num % 3 == 0)
             {
                 return "Fizz";
             }
-
             if (num % 5 == 0)
             {
                 return "Buzz";
