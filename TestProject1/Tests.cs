ab21ff8360e07b8d9de9b0174c82cc7bce1802e1
runlei <rlwang@thoughtworks.com>
2019-05-08 18:36:19 +0800

    [Commit 01][Test failed]: When numbert is times of 3 should return Fizz test complete and failed

--- TestProject1/Tests.cs
+++ TestProject1/Tests.cs
@@ -3,12 +3,28 @@ using Xunit;
 
 namespace TestProject1
 {
+    
+    public class CustomPrint
+    {
+        public string GetValue(int num)
+        {
+            return String.Empty;
+        }
+
+        public void Print(int input)
+        {
+           Console.WriteLine(GetValue(input));
+        }
+    }
+    
     public class Tests
     {
         [Fact]
-        public void Test1()
+        public void when_number_is_times_of_3_return_Fizz()
         {
-            Assert.True(true);
+            var printer = new CustomPrint();
+            Assert.Equal("Fizz", printer.GetValue(3));
+            Assert.NotEqual("Fizz", printer.GetValue(4));
         }
     }
 }
