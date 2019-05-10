49833c7cb723ecc3b9c2ad317217fad6736542d3
runlei <rlwang@thoughtworks.com>
2019-05-10 19:01:01 +0800

    [parking lot commit 02] when parkinglot is no available user can't park car

--- TestProject1/ParkingLot.cs
+++ TestProject1/ParkingLot.cs
@@ -3,6 +3,14 @@ using Xunit;
 
 namespace TestProject1
 {
+    public class NoAvailableException : Exception
+    {
+        public string GetExceptionMessage()
+        {
+            return "No Available";
+        }
+    }
+
     public class Ticket
     {
 
@@ -24,6 +32,10 @@ namespace TestProject1
 
         public Ticket Park(Car car)
         {
+            if (IsAvailable == false)
+            {
+                throw new NoAvailableException();
+            }
             return new Ticket();
         }
     }
@@ -38,5 +50,14 @@ namespace TestProject1
             var ticket = parkingLot.Park(car);
             Assert.Equal("TestProject1.Ticket", ticket.GetType().ToString());
         }
+        
+        [Fact]
+        public void when_parking_lot_has_no_available_space_and_user_comes_to_park_then_user_cannot_get_a_ticket()
+        {
+            var parkingLot = new ParkingLot(false);
+            var car = new Car();
+            Assert.Throws<NoAvailableException>(() => parkingLot.Park(car));
+        }
+
     }
 }
